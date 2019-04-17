3b270a14fb4adf5814d38e649f35d8617d2f17ed
Jacque Jordan <jaj2777@truman.edu>
2019-04-17 17:39:37 -0400

    Write downloaded webpage to file

--- webscrapeBookData.py
+++ webscrapeBookData.py
@@ -3,3 +3,7 @@ import bs4, requests
 page = requests.get('https://www.librarything.com/z_books.php')
 page.raise_for_status()
     
+file = open('books.php', 'wb')
+for chunk in page.iter_content(100000):
+    file.write(chunk)
+file.close()
