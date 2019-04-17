030eaf69bce36d931797f24193172f27b862dabe
Jacque Jordan <jaj2777@truman.edu>
2019-04-17 19:55:46 -0400

    Parse webpage for desired data
    The data is stored into a list. Each element contains both the title
    and the author. This will require regex for placing in proper database
    cells. I chose to not scrape the data as separate elements of title
    and author because one of the books does not have an author. This
    causes the list to be offset (when intending to iterate over the list
    and using data[i] as title and data[i+1] as author.)

--- webscrapeBookData.py
+++ webscrapeBookData.py
@@ -2,8 +2,12 @@ import bs4, requests
 
 page = requests.get('https://www.librarything.com/z_books.php')
 page.raise_for_status()
-    
-file = open('books.php', 'wb')
-for chunk in page.iter_content(100000):
-    file.write(chunk)
-file.close()
+
+filename = 'books.php'
+with open(filename, 'wb') as file:
+    for chunk in page.iter_content(100000):
+        file.write(chunk)
+
+with open(filename, encoding='utf-8') as file:
+    soupFile = bs4.BeautifulSoup(file, "html.parser")
+    data = soupFile.select('td[width="60%"] li')
